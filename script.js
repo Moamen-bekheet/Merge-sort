92a2b7a35c48ba2dd2cb94265bbcd9fa9da8ed15
Moamen <Moamenbekheet98@gmail.com>
2022-09-12 03:04:30 +0200

    added a merger function

--- script.js
+++ script.js
@@ -10,4 +10,28 @@ function mergeSort(arr){
     }
 }
 
-console.log(mergeSort([1,2,3]))
+function mergeArrs(leftArr, rightArr){
+    let [i,j] = [0,0];
+    let sortedArr = [];
+    while((i<leftArr.length)&&(j<rightArr.length)){
+        if(leftArr[i]<=rightArr[j]){
+            sortedArr.push(leftArr[i])
+            i += 1; 
+        }
+        else{
+            sortedArr.push(rightArr[i]);
+            j += 1;
+        }
+    }
+    while((!(i<leftArr.length))&&(j<rightArr.length)){
+        sortedArr.push(rightArr[j]);
+        j += 1;
+    }
+    while((i<leftArr.length)&&(!(j<rightArr.length))){
+        sortedArr.push(rightArr[i]);
+        i += 1;
+    }
+    return sortedArr;
+}
+
+console.log(mergeSort([9,8,7,6,5,4,3,2,1]));
